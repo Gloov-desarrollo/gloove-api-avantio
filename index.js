8deca34c31f5a1c88b7d6c2579a78b042fc46cae
Pedro <pedro>
2024-11-18 11:18:39 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const axios = require('axios');
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors()); 
+
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API Avantio - Gloove');
+});
+
+// Trae todas las reservas
+app.get('/all-bookings', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.avantio.pro/pms/v2/bookings', {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Avantio-Auth': process.env.AVANTIO_AUTH_TOKEN,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(error.response?.status || 500).send(error.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
