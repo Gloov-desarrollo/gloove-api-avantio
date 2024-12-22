11730fa7ae42345f227ce1d058877e82f6eea76a
Pedro <pedro>
2024-12-22 12:12:03 -0300

    cambio port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 const axios = require('axios');
 require('dotenv').config();
 const cors = require('cors');
