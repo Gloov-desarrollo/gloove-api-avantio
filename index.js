8942342dc42c387175061022b66386aa05a05e1e
Pedro <pedro>
2024-12-22 15:13:38 -0300

    ignore node modules folder

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const axios = require('axios');
 require('dotenv').config();
+const fs = require("fs");
+const xml2js = require("xml2js");
 const cors = require('cors');
 app.use(cors()); 
 
@@ -48,7 +50,7 @@ app.get('/bookings/:id', async (req, res) => {
   }
 });
 
-// Trae reserva por ID
+// Trae detalles de una propiedad
 app.get('/accommodations/:id', async (req, res) => {
   const {id} = req.params;
   try {
@@ -62,6 +64,41 @@ app.get('/accommodations/:id', async (req, res) => {
   } catch (error) {
     console.error(error.message);
     res.status(error.response?.status || 500).send(error.message);
+  }
+});
+
+// Trae info de un huesped
+app.get('/huesped/:id', async (req, res) => {
+  const {id} = req.params;
+  try {
+    const response = await axios.get(`https://api.avantio.pro/pms/v2/guests/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Avantio-Auth': process.env.AVANTIO_AUTH_TOKEN,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(error.response?.status || 500).send(error.message);
+  }
+});
+
+// Convierto XML de viviendas (descriptions.xml) a JSON
+app.get("/viviendas-json", async (req, res) => {
+  try {
+    // leo xml
+    const xmlFilePath = "./assets/descriptions.xml"; 
+    const xmlData = fs.readFileSync(xmlFilePath, "utf-8");
+
+    // convierto a json
+    const parser = new xml2js.Parser();
+    const jsonData = await parser.parseStringPromise(xmlData);
+
+    res.json(jsonData);
+  } catch (error) {
+    console.error("Error processing XML:", error);
+    res.status(500).json({ error: "Failed to process XML file" });
   }
 });
 
